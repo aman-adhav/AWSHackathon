af9b88e4202b1cb040d9071f42f01597b746663e
KovalRomanK <romankoval10@gmail.com>
2020-03-31 13:16:29 -0400

    Register page basic layout

---
+++ web/components/uppy.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="Uppy-container" ref="uppyContainer"></div>
+</template>
+
+<style lang="scss">
+@import "@uppy/core/dist/style.min.css";
+@import "@uppy/dashboard/dist/style.min.css";
+@import "@uppy/webcam/dist/style.min.css";
+
+#Uppy-container {
+  .uppy-Dashboard-inner {
+    border-radius: 0;
+  }
+}
+</style>
+
+<script>
+import Uppy from "@uppy/core";
+import Dashboard from "@uppy/dashboard";
+import Webcam from "@uppy/webcam";
+import XHRUpload from "@uppy/xhr-upload";
+
+export default {
+  mounted() {
+    this.uppy = Uppy({
+      restrictions: { allowedFileTypes: ["image/jpeg", "image/png"] }
+    })
+      .use(Webcam, {
+        modes: ["picture"],
+        preferredImageMimeType: "image/jpeg",
+        facingMode: "environment"
+      })
+      .use(Dashboard, {
+        target: this.$refs.uppyContainer,
+        inline: true,
+        hideUploadButton: true,
+        theme: "light",
+        width: "100%",
+        plugins: ["Webcam"]
+      })
+      .use(XHRUpload, { endpoint: "https://api2.transloadit.com" });
+
+    this.uppy.on("file-added", file => this.$emit("file-added", file));
+    this.uppy.on("file-removed", file => this.$emit("file-removed", file));
+    this.uppy.on("complete", result => this.$emit("complete", result));
+  },
+  beforeDestroy() {
+    this.uppy.close();
+  }
+};
+</script>
