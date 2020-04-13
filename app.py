a7f88d31d6be179b31accf2652a5de081343e10b
Aman Adhav <amanadhav@gmail.com>
2020-04-13 17:35:48 -0400

    new commit

--- app.py
+++ app.py
@@ -6,42 +6,203 @@ from uuid import uuid4
 from flask_cors import CORS
 import requests
 from flask import Flask, jsonify, request
-import pymongo
-import gridfs
+import boto3
+import ast
+import decimal
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 CORS(app)
-client = pymongo.MongoClient("mongodb+srv://uofthacks:hack4th3sky@cluster0-hzrwe.mongodb.net/test?retryWrites=true&w=majority")
-admin_list = client["AWSHackathon"]
-fs = gridfs.GridFS(admin_list)
-thi_parties = admin_list["online_shops"]
-products = admin_list["id"]
+s3 = boto3.resource('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('products')
+sage_client = boto3.client('sagemaker-runtime')
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return '''
-    	<form method="POST" action="/products/text" enctype="multipart/form-data">
-    		<input type="text" name="profile_id">
-    		<input type="file" name="barcode">
-    		<input type="submit">
-    	<form>
-        '''
+	return '''<form method=POST enctype=multipart/form-data action="upload">
+		<input type=file name=myfile>
+		<input type=submit>
+		</form>
+	'''
+
+@app.route('/create_new_product', methods=["POST"])
+def create_new_product():
+    new_uuid = str(uuid4())
+    #s3.Bucket('thirdparty-image-bucket').put_object(new_uuid+"/")
+    table.put_item (Item={
+        'product_description': "Add Product Description",
+        'username': 'admin',
+        'product_id': new_uuid,
+        'product_name': "Add Product Name",
+        'product_price': 0,
+        'is_used_product': True,
+        'barcode': -00000000,
+        'stat': "In progress"
+    })
+
+    return jsonify({"product_id": new_uuid}), 200
+
+@app.route('/update/<id_>', methods=['POST'])
+def update_product(id_):
+    product_id = id_
+    val = request.get_data()
+    dict_str = val.decode("UTF-8")
+    values = ast.literal_eval(dict_str)
+    required = ['product_name', 'product_price', 'is_used_product', 'barcode', 'product_description', 'stat']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    try:
+        retrieve = table.update_item(
+
+            UpdateExpression="set product_name = :r, product_price=:p, is_used_product=:a, barcode=:l, product_description=:t, stat=:stat",
+            ExpressionAttributeValues={
+                ':r': values['product_name'],
+                ':p': decimal.Decimal(values['product_price']),
+                ':a': values["is_used_product"],
+                ':l': values["barcode"],
+                ":t": values["product_description"],
+                ":stat": values["stat"]
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        return "Done", 200
+    except ClientError as e:
+        print(e.response['Error']['Message'])
 
 
-@app.route('/products/<id_>', methods=['POST'])
-def product(id_):
-    print(request)
-    if "barcode" in request.files:
-        barcode = request.files["barcode"]
-        profile_id = request.form["profile_id"]
-        fs.put(barcode)
-        products.insert_one({"product_id": id_, "barcode_file": barcode.filename, "profile_id": profile_id})
+def removeEmptyString(dic):
+  if isinstance(dic, str):
+    if dic == "":
+      return None
+    else:
+      return dic
 
-    return "Done"
+  for e in dic:
+    if isinstance(dic[e], dict):
+      dic[e] = removeEmptyString(dic[e])
+    if (isinstance(dic[e], str) and dic[e] == ""):
+      dic[e] = None
+    if isinstance(dic[e], list):
+      for entry in dic[e]:
+        removeEmptyString(entry)
+  return dic
+
+
+def update_from_barcode(dict_values, id_):
+    dict_values = removeEmptyString(dict_values)
+    product_id = id_
+    update_expression = 'SET {}'.format(','.join(f'#{k}=:{k}' for k in dict_values))
+    expression_attribute_values = {f':{k}': v for k, v in dict_values.items()}
+    expression_attribute_names = {f'#{k}': k for k in dict_values}
+
+    response = table.update_item(
+        Key={'username': 'admin', 'product_id': product_id},
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=expression_attribute_values,
+        ExpressionAttributeNames=expression_attribute_names,
+        ReturnValues='UPDATED_NEW',
+    )
+
+#todo
+@app.route('/delete/<id_>', methods=["POST"])
+def delete_item(id_):
+    image_list = request.get_data()
+    dict_str = image_list.decode("UTF-8")
+    values = ast.literal_eval(dict_str)
+    required = ['image_list']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    for url in values["image_list"]:
+        key = url.split("https://thirdparty-image-bucket.s3.amazonaws.com/")[1]
+        #s3.Bucket('thirdparty-image-bucket').delete_key(key)
+        s3.Object('thirdparty-image-bucket', key).delete()
+
+    return "Done", 200
+
+
+@app.route("/scan_barcode/<id_>", methods=["POST"])
+def scan_barcode(id_):
+    val = []
+    for filename, file in request.files.items():
+        #print(file.filename)
+        response = sage_client.invoke_endpoint(
+            EndpointName='barcode-reader-rat',
+            Body=file,
+            ContentType='image/jpeg',
+            Accept='application/json'
+        )
+
+        #print(response["Body"].read().decode("utf-8"))
+        val = response["Body"].read().decode("utf-8")
+        val = ast.literal_eval(val)
+
+    if len(list(val.keys())) == 1:
+        barcode = list(val.keys())[0]
+        value = {}
+        val = requests.get(
+            "https://api.barcodelookup.com/v2/products?barcode=" + barcode + "&formatted=y&key=0as2xduw3qreu9avhfv2pva2qz3rbm")
+        dict_str = val.content.decode("UTF-8")
+        if dict_str != "\n":
+            values = ast.literal_eval(dict_str)
+
+            json_body = values["products"][0]
+            value = {}
+            if "barcode_number" in json_body:
+                value["barcode"] = int(json_body["barcode_number"])
+
+            if "product_name" in json_body:
+                value["product_name"] = json_body["product_name"]
+
+            if "description" in json_body:
+                value['product_description'] = json_body['description']
+
+            if len(json_body["stores"]) > 1:
+                expensive = json_body["stores"][-1]["store_price"]
+                cheap = json_body["stores"][0]["store_price"]
+                value["expensive_version"] = expensive
+                value["cheap_version"] = cheap
+            elif len(json_body["stores"]) == 1:
+                value["version_in_market"] = json_body["stores"][0][
+                    "store_price"]
+
+            value["parse_product"] = json_body
+
+            #print(value)
+            update_from_barcode(value, id_)
+            return "Update Successful", 200
+
+        else:
+            return "Error Reading Barcode Please try again at another time", 200
+
+    else:
+        return "Unacceptable Input", 400
+
+
+@app.route('/retrieve/<id_>', methods=["POST"])
+def retrieve_images(id_):
+    folder_id = id_
+    my_bucket = s3.Bucket('thirdparty-image-bucket')
+    image_list = []
+    for object_summary in my_bucket.objects.filter(
+            Prefix="52ce57a6-6272-4e4e-91e5-d4bc0640a8bd/"):
+        val = "https://thirdparty-image-bucket.s3.amazonaws.com/"+ object_summary.key
+        image_list.append(val)
+    return jsonify({'image_list': image_list}), 200
+
+@app.route('/upload/<id_>', methods=['POST'])
+def upload(id_):
+    for filename, file in request.files.items():
+        #print(file.filename)
+        s3.Bucket('thirdparty-image-bucket').put_object(Key=id_+"/"+file.filename, Body=file)
+    return '<h1>File saved to S3 </h1>'
+
 
 
 if __name__ == '__main__':
+
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
--- barcode.py
+++ barcode.py
@@ -4,51 +4,21 @@ import boto3
 from sagemaker import get_execution_role
 from sagemaker import transformer
 from sagemaker import model
-from src.model_package_arns import ModelPackageArnProvider
+from AWSHackathon.src.model_package_arns import ModelPackageArnProvider
 from sagemaker import ModelPackage
-role = get_execution_role()
 
-sess = sage.Session()
 
-account = sess.boto_session.client('sts').get_caller_identity()['Account']
-region = sess.boto_session.region_name
+client = boto3.client('sagemaker-runtime')
 
-modelpackage_arn = ModelPackageArnProvider.get_model_package_arn(sess.boto_region_name)
+file = open("data/test_files/QRcode_example5.jpg", "rb")
 
-# Define predictor wrapper class
-def predict_wrapper(endpoint, session):
-    return sage.RealTimePredictor(endpoint, session, content_type='image/jpeg')
+response = client.invoke_endpoint(
+    EndpointName='barcode-reader-rat',
+    Body=file,
+    ContentType='image/jpeg',
+    Accept='application/json'
+)
 
-# Create a deployable model
-model = ModelPackage(role=role,
-                                      model_package_arn = modelpackage_arn,
-                                      sagemaker_session = sess,
-                                      predictor_cls = predict_wrapper)
-# ARN
-print(modelpackage_arn)
-file_name = 'data/test_files/my_barcode.jpg'
-with open(file_name, "rb") as image:
-  b = bytearray(image.read())
+print(response["ContentType"])
+print(response["Body"].read().decode("utf-8"))
 
-# Perform a prediction
-result = predictor.predict(b).decode('utf-8')
-
-# View the prediction
-print(result)
-
-bucket = sess.default_bucket()
-
-transform_input_prefix = "barcode/validation"
-TRANSFORM_WORKDIR = "data/transform"
-# upload data from local directory to bucket
-
-transform_input = sess.upload_data(TRANSFORM_WORKDIR, key_prefix=transform_input_prefix)
-
-print ("Transform Data Location " + transform_input)
-
-transformer = model.transformer(1, 'ml.m4.xlarge',
-                                output_path="s3://"+bucket+"/barcode/batch-transform-output")
-transformer.transform(transform_input, content_type='image/jpeg')
-transformer.wait()
-
-print("Batch Transform output saved to " + transformer.output_path)
